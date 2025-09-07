bf55faee6f3c63a9e427ea24a03afa433312e4c8
mika1407 <koneautomaatio@hotmail.com>
2025-09-08 01:21:04 +0300

    use props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import Header from './componets/Header'
 import Entry from './componets/Entry'
-import Entry2 from './componets/Entry2'
 
 
 function App() {
   return (
     <>
       <Header />
-      <Entry />
-      <Entry2 />
+      <main className="container">
+        <Entry img={{
+                src: "https://scrimba.com/links/travel-journal-japan-image-url", 
+                alt: "Mount Fuji"
+                }}
+                title="Mount Fuji"
+                location="Japan"
+                googleMapsUrl="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
+                dates="12 Jan, 2021 - 24 Jan, 2021"
+                description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
+      </main>
     </>
   )
 }
